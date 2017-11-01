709e1342aff6ecab8e267803e0a5ffd013019e67
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-01 20:53:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = nil
+  lowest_value = Float::INFINITY
   name_hash.map do |key, value|
-    if lowest_value == nil || value < lowest_value
+    if value < lowest_value
       lowest_value = value
       lowest_key = key
     end
